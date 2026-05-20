7ccf9f98e33f1c11ac11624eb12e8754ee6e6917
Majedul Hasan <hasanmajedul@gmail.com>
2026-05-20 19:20:17 +0600

    feat(logging): implement structured logging with Winston and middleware for request and error logging

--- src/config/token/generateToken.ts
+++
@@ -1,8 +0,0 @@
-import config from "@conf/env.const";
-import jwt from "jsonwebtoken"
-
-const generateToken = (id: string) => {
-  return jwt.sign({ id }, config.jwt.jwt_secret , { expiresIn: "20d" });
-};
-
-export default generateToken;
