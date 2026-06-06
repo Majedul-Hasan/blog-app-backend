5dc356de487d93a1effb02f9c2d2f11e466d91f6
Majedul Hasan <hasanmajedul@gmail.com>
2026-06-06 20:46:58 +0600

    refactor(reset-password.usecase): improve code formatting and streamline constructor and method structure

--- src/modules/auth/application/use-cases/reset-password.usecase.ts
+++ src/modules/auth/application/use-cases/reset-password.usecase.ts
@@ -1,61 +1,41 @@
 // src/modules/auth/application/use-cases/reset-password.usecase.ts
 
-import AppError from "@shared/errors/base/AppError";
+import AppError from '@shared/errors/base/AppError';
 
-import { UserRepository }
-    from "@modules/user/domain/user.repository.interface";
+import { UserRepository } from '@modules/user/domain/user.repository.interface';
 
-import { PasswordHasher }
-    from "@shared/security/password-hasher.interface";
+import { PasswordHasher } from '@shared/security/password-hasher.interface';
 
-import { ITokenProvider }
-    from "@shared/security/interfaces/token-provider.interface";
-import { UserNotFoundError } from "@shared/errors";
-import { ResetPasswordAuthDto } from "../dto/reset-password.dto";
+import { ITokenProvider } from '@shared/security/interfaces/token-provider.interface';
+import { UserNotFoundError } from '@shared/errors';
+import { ResetPasswordAuthDto } from '../dto/reset-password.dto';
 
 interface ResetPasswordPayload {
-    userId: string;
-    email: string;
+  userId: string;
+  email: string;
 }
 
 export class ResetPasswordUseCase {
+  constructor(
+    private readonly userRepo: UserRepository,
+    private readonly hasher: PasswordHasher,
+    private readonly tokenProvider: ITokenProvider
+  ) {}
 
-    constructor(
-        private readonly userRepo: UserRepository,
+  async execute(dto: ResetPasswordAuthDto): Promise<void> {
+    const decoded = this.tokenProvider.verify<ResetPasswordPayload>(dto.token);
 
-        private readonly hasher: PasswordHasher,
+    const user = await this.userRepo.findById(decoded.userId);
 
-        private readonly tokenProvider:
-            ITokenProvider
-    ) { }
-
-    async execute(
-        dto: ResetPasswordAuthDto
-    ): Promise<void> {
-
-        const decoded =
-            this.tokenProvider.verify<
-                ResetPasswordPayload
-            >(dto.token);
-
-        const user =
-            await this.userRepo.findById(
-                decoded.userId
-            );
-
-        if (!user) {
-            throw new UserNotFoundError()
-        }
-
-        const hashedPassword =
-            await this.hasher.hash(
-                dto.newPassword
-            );
-
-        user.changePassword(
-            hashedPassword
-        );
-
-        await this.userRepo.save(user);
+    if (!user) {
+      throw new UserNotFoundError();
     }
+
+    const hashedPassword = await this.hasher.hash(dto.newPassword);
+
+    console.log(36, hashedPassword);
+    user.changePassword(hashedPassword);
+
+    await this.userRepo.update(user.id, user);
+  }
 }
