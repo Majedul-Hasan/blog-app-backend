f7f5b0117a66dd86b6b58c4bc096e5de2c6baa66
Majedul Hasan <hasanmajedul@gmail.com>
2026-06-06 20:33:33 +0600

    refactor(jwt.provider): improve error handling and code formatting in JwtTokenProvider

--- src/infrastructure/security/jwt/jwt.provider.ts
+++ src/infrastructure/security/jwt/jwt.provider.ts
@@ -1,38 +1,27 @@
-import { ITokenProvider, GenerateTokenOptions } from "@shared/security/interfaces/token-provider.interface";
-import jwt, { Secret, SignOptions } from "jsonwebtoken";
+import { InvalidTokenError, TokenExpiredError } from '@shared/errors';
+import { ITokenProvider, GenerateTokenOptions } from '@shared/security/interfaces/token-provider.interface';
+import jwt, { Secret, SignOptions } from 'jsonwebtoken';
 
-export class JwtTokenProvider
-    implements ITokenProvider {
+export class JwtTokenProvider implements ITokenProvider {
+  constructor(private readonly secret: Secret) {}
 
-    constructor(
-        private readonly secret: Secret
-    ) { }
+  generate<T extends object>(payload: T, options?: GenerateTokenOptions): string {
+    return jwt.sign(payload, this.secret, {
+      algorithm: 'HS256',
+      expiresIn: options?.expiresIn as SignOptions['expiresIn'],
+    });
+  }
 
-    generate<T extends object>(
-        payload: T,
-        options?: GenerateTokenOptions
-    ): string {
-        return jwt.sign(payload, this.secret, {
-            algorithm: "HS256",
-            expiresIn:
-                options?.expiresIn as SignOptions["expiresIn"],
-        });
+  verify<T extends object>(token: string): T {
+    try {
+      const decoded = jwt.verify(token, this.secret);
+      if (typeof decoded === 'string') {
+        throw new InvalidTokenError('Invalid token payload');
+      }
+
+      return decoded as T;
+    } catch (error) {
+      throw new TokenExpiredError('Invalid or expired token');
     }
-
-    verify<T extends object>(
-        token: string
-    ): T {
-        const decoded = jwt.verify(
-            token,
-            this.secret
-        );
-
-        if (typeof decoded === "string") {
-            throw new Error(
-                "Invalid token payload"
-            );
-        }
-
-        return decoded as T;
-    }
+  }
 }
