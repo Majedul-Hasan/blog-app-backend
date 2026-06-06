876f60a8c3c6ae8c912f939d4b09e041948ef198
Majedul Hasan <hasanmajedul@gmail.com>
2026-06-06 20:46:26 +0600

    refactor(register-user.usecase): replace generic error with UserExistsError and improve code formatting

--- src/modules/user/application/use-cases/register-user.usecase.ts
+++ src/modules/user/application/use-cases/register-user.usecase.ts
@@ -1,34 +1,34 @@
-import { UserRepository } from "@modules/user/domain/user.repository.interface";
-import { RegisterUserDTO } from "../dto/register-user.dto";
-import { UserEntity } from "@modules/user/domain/user.entity";
-import { PasswordHasher } from "@shared/security/password-hasher.interface";
-import { BcryptPasswordHasher } from "@shared/security/bcrypt-password-hasher";
+import { UserRepository } from '@modules/user/domain/user.repository.interface';
+import { RegisterUserDTO } from '../dto/register-user.dto';
+import { UserEntity } from '@modules/user/domain/user.entity';
+import { PasswordHasher } from '@shared/security/password-hasher.interface';
+import { BcryptPasswordHasher } from '@shared/security/bcrypt-password-hasher';
+import { UserExistsError } from '@shared/errors';
+import { UserRole } from '@modules/user/domain/types/userRole.types';
 
 export class RegisterUserUseCase {
-    constructor(
-        private readonly userRepo: UserRepository,
-        private readonly hasher: PasswordHasher
-    ) { }
+  constructor(
+    private readonly userRepo: UserRepository,
+    private readonly hasher: PasswordHasher
+  ) {}
 
-    async execute(dto: RegisterUserDTO) {
-        const existing = await this.userRepo.findByEmail(dto.email);
-        console.log({ existing })
+  async execute(dto: RegisterUserDTO) {
+    const existing = await this.userRepo.findByEmail(dto.email);
+    console.log({ existing });
 
-        if (existing) throw new Error("user already exists");
+    if (existing) throw new UserExistsError('user already exists');
 
-        const passwordHash = await this.hasher.hash(dto.password);
-        console.log("passwordHash", passwordHash)
+    const passwordHash = await this.hasher.hash(dto.password);
+    console.log('passwordHash', passwordHash);
 
-        const user = UserEntity.create({
-            firstName: dto.firstName,
-            lastName: dto.lastName,
-            email: dto.email,
-            password: passwordHash,
-            role: "Guest",
-        });
-        console.log(29, user)
-        return await this.userRepo.save(user);
-
-
-    }
+    const user = UserEntity.create({
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      email: dto.email,
+      password: passwordHash,
+      role: UserRole.GUEST,
+    });
+    console.log(29, user);
+    return await this.userRepo.save(user);
+  }
 }
