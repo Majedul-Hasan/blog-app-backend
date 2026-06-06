b52ee41970358f0cb502d266e17ba9ca0dc1e37b
Majedul Hasan <hasanmajedul@gmail.com>
2026-06-06 20:47:37 +0600

    refactor(login-user.usecase): replace generic error with InvalidCredentialsError and improve code formatting

--- src/modules/user/application/use-cases/login-user.usecase.ts
+++ src/modules/user/application/use-cases/login-user.usecase.ts
@@ -1,43 +1,39 @@
-
-
-import { UserRepository } from "@modules/user/domain/user.repository.interface";
-import { UserEntity } from "@modules/user/domain/user.entity";
-import { PasswordHasher } from "@shared/security/password-hasher.interface";
-import { LoginUserDTO } from "../dto/login-user.dto";
-import { TokenService } from "@shared/security/token.service";
+import { UserRepository } from '@modules/user/domain/user.repository.interface';
+import { UserEntity } from '@modules/user/domain/user.entity';
+import { PasswordHasher } from '@shared/security/password-hasher.interface';
+import { LoginUserDTO } from '../dto/login-user.dto';
+import { TokenService } from '@shared/security/token.service';
+import { InvalidCredentialsError } from '@shared/errors';
 
 export class LoginUserUseCase {
-    constructor(
-        private readonly userRepo: UserRepository,
-        private readonly hasher: PasswordHasher,
-        private readonly tokenService: TokenService
-    ) { }
+  constructor(
+    private readonly userRepo: UserRepository,
+    private readonly hasher: PasswordHasher,
+    private readonly tokenService: TokenService
+  ) {}
 
-    async execute(dto: LoginUserDTO) {
-        const user = await this.userRepo.findByEmail(dto.email);
-        if (!user) throw new Error("Invalid email or password");
-        console.log(user)
+  async execute(dto: LoginUserDTO) {
+    const user = await this.userRepo.findByEmail(dto.email);
+    if (!user) throw new InvalidCredentialsError('Invalid email or password');
+    console.log(user);
 
-        const isMatch = await this.hasher.compare(
-            dto.password,
-            user.password
-        );
+    const isMatch = await this.hasher.compare(dto.password, user.password);
 
-        if (!isMatch) throw new Error("Invalid email or password");
+    if (!isMatch) throw new InvalidCredentialsError('Invalid email or password');
 
-        const payload = {
-            userId: user.id!,
-            email: user.email,
-            role: user.role,
-        };
+    const payload = {
+      userId: user.id!,
+      email: user.email,
+      role: user.role,
+    };
 
-        const accessToken = this.tokenService.generateAccessToken(payload);
-        const refreshToken = this.tokenService.generateRefreshToken(payload);
+    const accessToken = this.tokenService.generateAccessToken(payload);
+    const refreshToken = this.tokenService.generateRefreshToken(payload);
 
-        return {
-            user,
-            accessToken,
-            refreshToken,
-        };
-    }
+    return {
+      user,
+      accessToken,
+      refreshToken,
+    };
+  }
 }
