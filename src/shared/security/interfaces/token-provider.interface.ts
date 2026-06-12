c919b6576b5796d595b462722a50dc0dea4d9b0b
Majedul Hasan <hasanmajedul@gmail.com>
2026-06-12 12:04:35 +0600

    refactor(token-provider.interface): improve interface structure and naming for clarity

--- src/shared/security/interfaces/token-provider.interface.ts
+++ src/shared/security/interfaces/token-provider.interface.ts
@@ -1,23 +1,18 @@
-
 export interface TokenPayload {
-    userId: string;
-    email: string;
-    role: string;
+  userId: string;
+  email: string;
+  role: string;
 }
 
 export interface GenerateTokenOptions {
-    expiresIn?: string | number;
-    //   expiresIn?: SignOptions["expiresIn"];
+  expiresIn?: string | number;
+  //   expiresIn?: SignOptions["expiresIn"];
 }
 
 export interface ITokenProvider {
-    generate<T extends object>(
-        payload: T,
-        options?: GenerateTokenOptions
-    ): string;
+  generateAccessToken<T extends object>(payload: T, options?: GenerateTokenOptions): string;
+  generateRefreshToken<T extends object>(payload: T, options?: GenerateTokenOptions): string;
 
-    verify<T extends object>(token: string): T;
+  verifyAccessToken<T extends object>(token: string): T;
+  verifyRefreshToken<T extends object>(token: string): T;
 }
-
-
-
