8b3038a1b6b957f064aa4c699b29cc72886b856b
Majedul Hasan <hasanmajedul@gmail.com>
2026-06-12 11:56:58 +0600

    refactor(auth.middleware): improve code formatting and structure for readability

--- src/infrastructure/http/express/middlewares/auth.middleware.ts
+++ src/infrastructure/http/express/middlewares/auth.middleware.ts
@@ -1,47 +1,25 @@
-// infrastructure/http/express/middlewares/auth.middleware.ts
+import { Request, Response, NextFunction } from 'express';
 
-import { Request, Response, NextFunction } from "express";
+import { extractBearerToken } from '@shared/security/extractBearerToken';
+import { ForbiddenError } from '@shared/errors';
+import { AuthenticationService } from '@modules/auth/application/services/authentication.service';
 
-import { extractBearerToken } from "@shared/security/extractBearerToken";
-import { ForbiddenError } from "@shared/errors";
-import { AuthenticationService } from "@modules/auth/application/services/authentication.service";
+export const authMiddleware = (authService: AuthenticationService, ...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = extractBearerToken(req.headers.authorization);
 
-;
+      const user = await authService.authenticate(token);
 
-export const authMiddleware = (
-    authService: AuthenticationService,
-    ...roles: string[]
-) => {
-    return async (
-        req: Request,
-        res: Response,
-        next: NextFunction
-    ) => {
-        try {
+      if (roles.length && !roles.includes(user.role)) {
+        throw new ForbiddenError();
+      }
 
-            const token =
-                extractBearerToken(
-                    req.headers.authorization
-                );
+      req.user = user;
 
-            const user =
-                await authService.authenticate(
-                    token
-                );
-
-            if (
-                roles.length &&
-                !roles.includes(user.role)
-            ) {
-                throw new ForbiddenError;
-            }
-
-            req.user = user;
-
-            next();
-
-        } catch (error) {
-            next(error);
-        }
-    };
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 };
